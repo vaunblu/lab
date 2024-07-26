b6234486e0e665e75104233050ac077fc5b67c8f
vaunblu <vaunblu@proton.me>
2024-07-26 09:21:28 -0400

    chore: add initial framer motion. need to perfect timings

--- src/app/_components/toolbar/index.tsx
+++ src/app/_components/toolbar/index.tsx
@@ -15,135 +15,163 @@ import {
 } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { DevSwitch } from "./dev-switch";
+import { AnimatePresence, motion, MotionConfig } from "framer-motion";
+import useMeasure from "react-use-measure";
 
 export function Toolbar() {
+  const [ref, bounds] = useMeasure();
   const [devMode, setDevMode] = React.useState(false);
   const [activeOption, setActiveOption] = React.useState("pointer");
 
   return (
-    <div className="flex gap-2 divide-x rounded-xl border shadow-lg">
-      {devMode ? (
-        <div className="flex items-center gap-2 p-2.5">
-          <div className="flex items-center gap-2">
-            <Button
-              size="icon"
-              variant={activeOption === "pointer" ? "default" : "ghost"}
-              onClick={() => setActiveOption("pointer")}
-              className="rounded-lg p-2.5"
-            >
-              <MousePointer2 className="size-6" />
-            </Button>
-            <ChevronDown className="size-3" />
-          </div>
+    <MotionConfig transition={{ type: "spring", bounce: 0, duration: 0.4 }}>
+      <motion.div
+        animate={{ width: bounds.width }}
+        className="relative flex gap-2 divide-x overflow-hidden rounded-xl border shadow-lg"
+      >
+        <div ref={ref} className="relative flex gap-2 divide-x">
+          <AnimatePresence initial={false} mode="popLayout">
+            {devMode ? (
+              <motion.div
+                key="dev-mode"
+                initial={{ y: 50 }}
+                animate={{ y: 0 }}
+                exit={{ y: 50 }}
+                className="flex items-center gap-2 p-2.5"
+              >
+                <div className="flex items-center gap-2">
+                  <Button
+                    size="icon"
+                    variant={activeOption === "pointer" ? "default" : "ghost"}
+                    onClick={() => setActiveOption("pointer")}
+                    className="rounded-lg p-2.5"
+                  >
+                    <MousePointer2 className="size-6" />
+                  </Button>
+                  <ChevronDown className="size-3" />
+                </div>
 
-          <Button
-            size="icon"
-            variant={activeOption === "ruler" ? "default" : "ghost"}
-            onClick={() => setActiveOption("ruler")}
-            className="rounded-lg p-2.5"
-          >
-            <Ruler className="size-6" />
-          </Button>
+                <Button
+                  size="icon"
+                  variant={activeOption === "ruler" ? "default" : "ghost"}
+                  onClick={() => setActiveOption("ruler")}
+                  className="rounded-lg p-2.5"
+                >
+                  <Ruler className="size-6" />
+                </Button>
 
-          <Button
-            size="icon"
-            variant={activeOption === "square-pen" ? "default" : "ghost"}
-            onClick={() => setActiveOption("square-pen")}
-            className="rounded-lg p-2.5"
-          >
-            <SquarePen className="size-6" />
-          </Button>
+                <Button
+                  size="icon"
+                  variant={activeOption === "square-pen" ? "default" : "ghost"}
+                  onClick={() => setActiveOption("square-pen")}
+                  className="rounded-lg p-2.5"
+                >
+                  <SquarePen className="size-6" />
+                </Button>
 
-          <Button
-            size="icon"
-            variant={activeOption === "message" ? "default" : "ghost"}
-            onClick={() => setActiveOption("message")}
-            className="rounded-lg p-2.5"
+                <Button
+                  size="icon"
+                  variant={activeOption === "message" ? "default" : "ghost"}
+                  onClick={() => setActiveOption("message")}
+                  className="rounded-lg p-2.5"
+                >
+                  <MessageCircle className="size-6" />
+                </Button>
+              </motion.div>
+            ) : (
+              <motion.div
+                key="design-mode"
+                initial={{ y: -50 }}
+                animate={{ y: 0 }}
+                exit={{ y: -50 }}
+                className="flex items-center gap-2 p-2.5"
+              >
+                <div className="flex items-center gap-2">
+                  <Button
+                    size="icon"
+                    variant={activeOption === "pointer" ? "default" : "ghost"}
+                    onClick={() => setActiveOption("pointer")}
+                    className="rounded-lg p-2.5"
+                  >
+                    <MousePointer2 className="size-6" />
+                  </Button>
+                  <ChevronDown className="size-3" />
+                </div>
+
+                <div className="r flex items-center gap-2">
+                  <Button
+                    size="icon"
+                    variant={activeOption === "frame" ? "default" : "ghost"}
+                    onClick={() => setActiveOption("frame")}
+                    className="rounded-lg p-2.5"
+                  >
+                    <Frame className="size-6" />
+                  </Button>
+                  <ChevronDown className="size-3" />
+                </div>
+
+                <div className="r flex items-center gap-2">
+                  <Button
+                    size="icon"
+                    variant={activeOption === "square" ? "default" : "ghost"}
+                    onClick={() => setActiveOption("square")}
+                    className="rounded-lg p-2.5"
+                  >
+                    <Square className="size-6" />
+                  </Button>
+                  <ChevronDown className="size-3" />
+                </div>
+
+                <div className="r flex items-center gap-2">
+                  <Button
+                    size="icon"
+                    variant={activeOption === "pen-tool" ? "default" : "ghost"}
+                    onClick={() => setActiveOption("pen-tool")}
+                    className="rounded-lg p-2.5"
+                  >
+                    <PenTool className="size-6" />
+                  </Button>
+                  <ChevronDown className="size-3" />
+                </div>
+
+                <Button
+                  size="icon"
+                  variant={activeOption === "type" ? "default" : "ghost"}
+                  onClick={() => setActiveOption("type")}
+                  className="rounded-lg p-2.5"
+                >
+                  <Type className="size-6" />
+                </Button>
+
+                <Button
+                  size="icon"
+                  variant={activeOption === "message" ? "default" : "ghost"}
+                  onClick={() => setActiveOption("message")}
+                  className="rounded-lg p-2.5"
+                >
+                  <MessageCircle className="size-6" />
+                </Button>
+
+                <Button
+                  size="icon"
+                  variant={activeOption === "more" ? "default" : "ghost"}
+                  onClick={() => setActiveOption("more")}
+                  className="rounded-lg p-2.5"
+                >
+                  <Blocks className="size-6" />
+                </Button>
+              </motion.div>
+            )}
+          </AnimatePresence>
+
+          <motion.div
+            layout
+            className="flex items-center justify-center bg-background p-2.5"
           >
-            <MessageCircle className="size-6" />
-          </Button>
+            <DevSwitch checked={devMode} onCheckedChange={setDevMode} />
+          </motion.div>
         </div>
-      ) : (
-        <div className="flex items-center gap-2 p-2.5">
-          <div className="flex items-center gap-2">
-            <Button
-              size="icon"
-              variant={activeOption === "pointer" ? "default" : "ghost"}
-              onClick={() => setActiveOption("pointer")}
-              className="rounded-lg p-2.5"
-            >
-              <MousePointer2 className="size-6" />
-            </Button>
-            <ChevronDown className="size-3" />
-          </div>
-
-          <div className="r flex items-center gap-2">
-            <Button
-              size="icon"
-              variant={activeOption === "frame" ? "default" : "ghost"}
-              onClick={() => setActiveOption("frame")}
-              className="rounded-lg p-2.5"
-            >
-              <Frame className="size-6" />
-            </Button>
-            <ChevronDown className="size-3" />
-          </div>
-
-          <div className="r flex items-center gap-2">
-            <Button
-              size="icon"
-              variant={activeOption === "square" ? "default" : "ghost"}
-              onClick={() => setActiveOption("square")}
-              className="rounded-lg p-2.5"
-            >
-              <Square className="size-6" />
-            </Button>
-            <ChevronDown className="size-3" />
-          </div>
-
-          <div className="r flex items-center gap-2">
-            <Button
-              size="icon"
-              variant={activeOption === "pen-tool" ? "default" : "ghost"}
-              onClick={() => setActiveOption("pen-tool")}
-              className="rounded-lg p-2.5"
-            >
-              <PenTool className="size-6" />
-            </Button>
-            <ChevronDown className="size-3" />
-          </div>
-
-          <Button
-            size="icon"
-            variant={activeOption === "type" ? "default" : "ghost"}
-            onClick={() => setActiveOption("type")}
-            className="rounded-lg p-2.5"
-          >
-            <Type className="size-6" />
-          </Button>
-
-          <Button
-            size="icon"
-            variant={activeOption === "message" ? "default" : "ghost"}
-            onClick={() => setActiveOption("message")}
-            className="rounded-lg p-2.5"
-          >
-            <MessageCircle className="size-6" />
-          </Button>
-
-          <Button
-            size="icon"
-            variant={activeOption === "more" ? "default" : "ghost"}
-            onClick={() => setActiveOption("more")}
-            className="rounded-lg p-2.5"
-          >
-            <Blocks className="size-6" />
-          </Button>
-        </div>
-      )}
-      <div className="flex items-center justify-center p-2.5">
-        <DevSwitch checked={devMode} onCheckedChange={setDevMode} />
-      </div>
-    </div>
+      </motion.div>
+    </MotionConfig>
   );
 }
