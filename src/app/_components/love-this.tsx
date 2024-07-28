f7cfc4a229d2cb05e80f1aae8d3cdcdf4d39f1ee
vaunblu <vaunblu@proton.me>
2024-07-28 06:40:42 -0400

    chore: configure the rate button animation

--- src/app/_components/love-this.tsx
+++ src/app/_components/love-this.tsx
@@ -3,9 +3,8 @@
 import * as React from "react";
 import * as Popover from "@radix-ui/react-popover";
 import { Check, Send, ThumbsDown, ThumbsUp, X } from "lucide-react";
-import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
-import { AnimatePresence, motion } from "framer-motion";
+import { AnimatePresence, motion, MotionConfig } from "framer-motion";
 
 export const RatePopoverContext = React.createContext<{
   open: boolean;
@@ -19,80 +18,92 @@ function RatePopover() {
   const ctx = React.useContext(RatePopoverContext);
 
   return (
-    <Popover.Root open={ctx.open} onOpenChange={ctx.setOpen}>
-      <Popover.Trigger asChild>
-        <Button
-          variant="ghost"
-          size="icon"
-          className="size-14 rounded-full p-2"
-        >
-          <ThumbsUp
-            className={cn(
-              "size-8 cursor-pointer transition-opacity",
-              ctx.open ? "opacity-0" : "opacity-100",
-            )}
-          />
-        </Button>
-      </Popover.Trigger>
-
-      <AnimatePresence initial={false}>
-        {ctx.open && (
-          <Popover.Portal forceMount>
-            <>
-              <Popover.Content
-                side="top"
-                sideOffset={20}
-                onInteractOutside={(e) => e.preventDefault()}
-                asChild
+    <MotionConfig transition={{ type: "spring", bounce: 0, duration: 0.4 }}>
+      <Popover.Root open={ctx.open} onOpenChange={ctx.setOpen}>
+        <Popover.Trigger>
+          <AnimatePresence initial={false}>
+            {!ctx.open ? (
+              <motion.div
+                initial={{ y: -10, opacity: 0 }}
+                animate={{ y: 0, opacity: 1 }}
+                exit={{ y: -10, opacity: 0 }}
+                className="flex flex-col items-center gap-2"
               >
-                <motion.div
-                  initial={{ y: 10, opacity: 0 }}
-                  animate={{ y: 0, opacity: 1 }}
-                  exit={{ y: 10, opacity: 0 }}
-                  className="relative z-50 flex items-end gap-4 rounded-full border bg-popover px-8 pb-4 pt-3 font-semibold text-popover-foreground shadow-md outline-none"
-                >
-                  <div className="flex flex-col items-center gap-2">
-                    <Button
-                      variant="ghost"
-                      size="icon"
-                      className="size-16 rounded-full p-2"
-                    >
-                      <ThumbsDown className="size-10" />
-                    </Button>
-                    <p className="text-nowrap">Not for me</p>
-                  </div>
-                  <div className="flex flex-col items-center gap-2.5">
-                    <Button
-                      variant="ghost"
-                      size="icon"
-                      className="size-16 rounded-full p-2"
-                    >
-                      <ThumbsUp className="size-10" />
-                    </Button>
-                    <p className="text-nowrap">I like this</p>
-                  </div>
-                  <div className="flex flex-col items-center gap-2">
-                    <Button
-                      variant="ghost"
-                      size="icon"
-                      className="size-16 rounded-full p-2"
-                    >
-                      <ThumbsUp className="size-10" />
-                    </Button>
-                    <p className="text-nowrap">Love this!</p>
-                  </div>
-                  <Popover.Close className="absolute -bottom-[68px] left-1/2 flex -translate-x-1/2 items-center justify-center rounded-full bg-white p-1.5 shadow-md">
-                    <X className="size-7" />
-                  </Popover.Close>
-                </motion.div>
-              </Popover.Content>
+                <div className="size-14 inline-flex items-center justify-center whitespace-nowrap rounded-full p-2 text-sm font-medium ring-offset-background transition-colors hover:bg-accent hover:text-accent-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50">
+                  <ThumbsUp className="size-8 cursor-pointer" />
+                </div>
+                <p>Rate</p>
+              </motion.div>
+            ) : (
+              <div className="h-[88px] w-[56px]" />
+            )}
+          </AnimatePresence>
+        </Popover.Trigger>
 
-              <div className="size-full absolute left-0 top-0 z-40 bg-black opacity-25 transition-opacity duration-200" />
-            </>
-          </Popover.Portal>
-        )}
-      </AnimatePresence>
-    </Popover.Root>
+        <AnimatePresence initial={false}>
+          {ctx.open && (
+            <Popover.Portal forceMount>
+              <>
+                <Popover.Content
+                  side="top"
+                  sideOffset={20}
+                  onInteractOutside={(e) => e.preventDefault()}
+                  asChild
+                >
+                  <motion.div
+                    initial={{ y: 10, opacity: 0 }}
+                    animate={{ y: 0, opacity: 1 }}
+                    exit={{ y: 10, opacity: 0 }}
+                    className="relative z-50 flex items-end gap-4 rounded-full border bg-popover px-8 pb-4 pt-3 font-semibold text-popover-foreground shadow-md outline-none"
+                  >
+                    <div className="flex flex-col items-center gap-2">
+                      <Button
+                        variant="ghost"
+                        size="icon"
+                        className="size-16 rounded-full p-2"
+                      >
+                        <ThumbsDown className="size-10" />
+                      </Button>
+                      <p className="text-nowrap">Not for me</p>
+                    </div>
+                    <div className="flex flex-col items-center gap-2.5">
+                      <Button
+                        variant="ghost"
+                        size="icon"
+                        className="size-16 rounded-full p-2"
+                      >
+                        <ThumbsUp className="size-10" />
+                      </Button>
+                      <p className="text-nowrap">I like this</p>
+                    </div>
+                    <div className="flex flex-col items-center gap-2">
+                      <Button
+                        variant="ghost"
+                        size="icon"
+                        className="size-16 rounded-full p-2"
+                      >
+                        <ThumbsUp className="size-10" />
+                      </Button>
+                      <p className="text-nowrap">Love this!</p>
+                    </div>
+                    <Popover.Close className="absolute -bottom-[68px] left-1/2 flex -translate-x-1/2 items-center justify-center rounded-full bg-white p-1.5 shadow-md">
+                      <X className="size-7" />
+                    </Popover.Close>
+                  </motion.div>
+                </Popover.Content>
+
+                <motion.div
+                  initial={{ opacity: 0 }}
+                  animate={{ opacity: 0.3 }}
+                  exit={{ opacity: 0 }}
+                  className="size-full absolute left-0 top-0 z-40 bg-black"
+                />
+              </>
+            </Popover.Portal>
+          )}
+        </AnimatePresence>
+      </Popover.Root>
+    </MotionConfig>
   );
 }
 
@@ -108,17 +119,9 @@ export function LoveThis() {
           </Button>
           <p>My List</p>
         </div>
-        <div className="flex flex-col items-center gap-2">
-          <RatePopover />
-          <p
-            className={cn(
-              "transition-opacity",
-              open ? "opacity-0" : "opacity-100",
-            )}
-          >
-            Rate
-          </p>
-        </div>
+
+        <RatePopover />
+
         <div className="flex flex-col items-center gap-2">
           <Button
             variant="ghost"
