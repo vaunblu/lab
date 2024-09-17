4e1f675bbe030bd17d066b6b1985d8715a723fd0
vaunblu <vaunblu@proton.me>
2024-09-17 09:58:51 -0400

    feat: add shop claim

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -6,6 +6,7 @@ import { Button } from "@/components/ui/button";
 import { ScrollArea } from "@/components/ui/scroll-area";
 
 const examples = [
+  "shop-claim",
   "love-folder",
   "tooltip-nav",
   "audiobooks",
