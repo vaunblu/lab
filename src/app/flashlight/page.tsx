a1de0c98dd8bc6ce910bb2a37b43ef1a38cfad7b
vaunblu <vaunblu@proton.me>
2024-09-18 14:04:30 -0400

    flashlight 4

--- src/app/flashlight/page.tsx
+++ src/app/flashlight/page.tsx
@@ -15,11 +15,11 @@ function InnerContent() {
 
   return (
     <div className="relative h-[300px] w-[600px] overflow-visible">
-      <div className="size-full absolute bottom-0 left-0 bg-[radial-gradient(circle,#dcd8ff_0%,transparent_100%)] bg-[size:100%_75%] bg-[position:50%_90%] bg-no-repeat opacity-50 blur-xl [mask:radial-gradient(circle_at_center,black_0%,transparent_70%)]" />
+      <div className="size-full absolute bottom-0 left-0 bg-[radial-gradient(circle,#dcd8ff_0%,transparent_100%)] bg-[size:60%_75%] bg-[position:50%_90%] bg-no-repeat opacity-60 blur-lg [mask:radial-gradient(circle_at_center,black_0%,transparent_70%)]" />
       <div className="absolute bottom-[30px] left-[calc(50%-50px)] h-[250px] w-[150px] origin-bottom -rotate-[60deg] bg-[linear-gradient(90deg,red_0%,orange_30%,yellow_40%,green_45%,blue_80%,purple_100%)] opacity-20 blur-xl [mask:radial-gradient(circle_at_center,black_0%,transparent_70%)]" />
       <div className="absolute bottom-[30px] right-[calc(50%-50px)] h-[250px] w-[150px] origin-bottom rotate-[60deg] bg-[linear-gradient(270deg,red_0%,orange_30%,yellow_40%,green_45%,blue_80%,purple_100%)] opacity-20 blur-xl [mask:radial-gradient(circle_at_center,black_0%,transparent_70%)]" />
-      <div className="absolute bottom-0 left-1/2 h-[250px] w-[500px] -translate-x-1/2 bg-[radial-gradient(farthest-side_at_bottom,#dcd8ff_0%,transparent_100%)] blur-lg [mask:conic-gradient(from_-65deg_at_bottom,transparent_0deg,rgba(0,0,0,0.5)_20deg_110deg,transparent_130deg)]" />
-      <div className="size-full absolute bottom-0 left-0 bg-[radial-gradient(circle,#f5f1ff_20%,#dcd8ff_100%)] bg-[size:50px_60px] bg-[position:50%_90%] bg-no-repeat opacity-100 blur-lg [mask:radial-gradient(circle_at_center,black_0%,transparent_70%)]" />
+      <div className="absolute bottom-0 left-1/2 h-[250px] w-[500px] -translate-x-1/2 bg-[radial-gradient(farthest-side_at_bottom,#dcd8ff_0%,transparent_100%)] blur-lg [mask:conic-gradient(from_-65deg_at_bottom,transparent_0deg,rgba(0,0,0,0.3)_20deg_110deg,transparent_130deg)]" />
+      <div className="size-full absolute bottom-0 left-0 bg-[radial-gradient(circle,#f5f1ff_10%,#dcd8ff_50%,transparent_90%)] bg-[size:70px_100px] bg-[position:50%_90%] bg-no-repeat opacity-60 blur-lg [mask:radial-gradient(circle_at_center,black_0%,transparent_100%)]" />
     </div>
   );
 }
