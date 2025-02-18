bc70625896b1b330b9ab1660d2c51f9c8fc28d42
vaunblu <vaunblu@proton.me>
2025-02-18 08:00:09 -0800

    chore: more work on the dropdown
    final touches

--- src/app/smooth-dropdown/page.tsx
+++ src/app/smooth-dropdown/page.tsx
@@ -4,29 +4,52 @@ import imageInterstellar from "./interstellar.jpg";
 import imageMurph from "./murph.jpg";
 
 import { cn } from "@/lib/utils";
-import {
-  AnimatePresence,
-  motion,
-  MotionConfig,
-  Transition,
-  Variants,
-} from "framer-motion";
+import { motion, MotionConfig, Transition, Variants } from "framer-motion";
 import React from "react";
 import Image from "next/image";
 import svgPhone from "@/assets/iphone-black.svg";
-import { History, SquarePen } from "lucide-react";
-import useMeasure from "react-use-measure";
+import {
+  Bolt,
+  ChevronUp,
+  Globe,
+  History,
+  Home,
+  SquarePen,
+  Star,
+  SwatchBook,
+} from "lucide-react";
 
-const transition: Transition = { type: "spring", bounce: 0, duration: 0.4 };
+const transition: Transition = { type: "spring", bounce: 0, duration: 0.8 };
 
 const Context = React.createContext<{
   status: string;
   setStatus: React.Dispatch<React.SetStateAction<string>>;
 }>({ status: "", setStatus: () => null });
 
+function DropdownItem(props: {
+  className?: string;
+  children: React.ReactNode;
+}) {
+  const dropdownItemVariants: Variants = {
+    closed: { y: -190, filter: "blur(0.5px)" },
+    open: { y: 0, filter: "blur(0px)" },
+  };
+
+  return (
+    <motion.div
+      variants={dropdownItemVariants}
+      className={cn(
+        "flex cursor-pointer items-center gap-3 rounded-full px-2 py-1 transition-colors duration-500 hover:bg-muted",
+        props.className,
+      )}
+    >
+      {props.children}
+    </motion.div>
+  );
+}
+
 function InnerContent() {
   const ctx = React.useContext(Context);
-  const [ref, bounds] = useMeasure();
 
   const isOpen = ctx.status === "open";
 
@@ -38,15 +61,104 @@ function InnerContent() {
     }
   }
 
+  const dropdownVariants: Variants = {
+    closed: {
+      height: 0,
+      borderColor: "rgba(225, 225, 225, 0)",
+      marginBottom: 0,
+    },
+    open: {
+      height: "auto",
+      borderColor: "rgba(225, 225, 225, 0.5)",
+      marginBottom: 32,
+    },
+  };
+
   return (
     <div className="relative flex h-full flex-col overflow-hidden rounded-[51px] bg-[#FAFAFA] font-medium text-[#1F1F1F]">
       <div className="flex w-full items-center justify-between px-6 pt-16">
-        <History className="size-5" />
-        <button onClick={handleMenuToggle}>menu</button>
-        <SquarePen className="size-5" />
+        <History strokeWidth={2.1} className="size-6" />
+        <motion.button
+          whileTap={{ scale: 0.9 }}
+          onClick={handleMenuToggle}
+          className="flex items-center gap-1.5 text-lg font-medium"
+        >
+          menu
+          <motion.span
+            initial={{ rotate: 0 }}
+            animate={isOpen ? { rotate: 180 } : {}}
+            transition={{ ...transition, type: "ease-out" }}
+          >
+            <ChevronUp
+              strokeWidth={3}
+              className="size-4 text-muted-foreground/80"
+            />
+          </motion.span>
+        </motion.button>
+        <SquarePen strokeWidth={2.1} className="size-6" />
       </div>
-      <motion.div animate={{ height: bounds.height }}>
-        <div ref={ref}></div>
+
+      <motion.div
+        variants={dropdownVariants}
+        initial="closed"
+        animate={isOpen ? "open" : "closed"}
+        transition={{
+          ...transition,
+          staggerChildren: isOpen ? 0.08 : 0,
+          staggerDirection: -1,
+        }}
+        className="z-10 mx-4 mt-4 border-t"
+      >
+        <div className="relative space-y-1 overflow-hidden pt-4">
+          <DropdownItem>
+            <Home
+              strokeWidth={2.5}
+              className="size-5 text-muted-foreground/80"
+            />
+            Return home
+          </DropdownItem>
+          <DropdownItem className="justify-between pr-1">
+            <div className="flex items-center gap-3">
+              <Star
+                strokeWidth={2.5}
+                className="size-5 text-muted-foreground/80"
+              />
+              Rating
+            </div>
+            <div className="rounded-full bg-primary px-2 py-0.5 text-sm text-primary-foreground">
+              5 stars
+            </div>
+          </DropdownItem>
+          <DropdownItem>
+            <Globe
+              strokeWidth={2.5}
+              className="size-5 text-muted-foreground/80"
+            />
+            Language
+          </DropdownItem>
+          <DropdownItem className="justify-between pr-1">
+            <div className="flex items-center gap-3">
+              <SwatchBook
+                strokeWidth={2.5}
+                className="size-5 text-muted-foreground/80"
+              />
+              Appearance
+            </div>
+            <div className="flex items-center rounded-full bg-muted-foreground/20 p-0.5 text-sm">
+              <div className="rounded-full bg-background px-2 py-0.5 ">
+                Light
+              </div>
+              <div className="rounded-full px-2 py-0.5 ">Dark</div>
+            </div>
+          </DropdownItem>
+          <DropdownItem>
+            <Bolt
+              strokeWidth={2.5}
+              className="size-5 text-muted-foreground/80"
+            />
+            Settings
+          </DropdownItem>
+        </div>
       </motion.div>
 
       <div className="relative flex h-screen flex-col bg-background px-6 py-4 leading-relaxed">
@@ -97,7 +209,7 @@ function InnerContent() {
 
       <motion.div
         initial={{ height: "20%" }}
-        animate={isOpen ? { height: "50%" } : {}}
+        animate={isOpen ? { height: "80%" } : {}}
         className="absolute bottom-0 left-0 w-full bg-gradient-to-t from-[#FAFAFA]"
       />
       <motion.div
@@ -110,7 +222,8 @@ function InnerContent() {
 }
 
 export default function HomePage() {
-  const [status, setStatus] = React.useState("idle");
+  const [status, setStatus] = React.useState("open");
+  // const [status, setStatus] = React.useState("idle");
 
   React.useEffect(() => {
     function handleEscape(e: KeyboardEvent) {
@@ -128,7 +241,7 @@ export default function HomePage() {
         <main className="flex h-screen select-none items-center justify-center overflow-hidden">
           <div
             className={cn(
-              "relative flex aspect-square h-screen items-center justify-center bg-gradient-to-br from-[#FFFFFF] from-20% to-[#EEEEEE] transition-colors duration-300",
+              "relative flex aspect-square h-screen items-center justify-center bg-gradient-to-br from-[#FFFFFF] from-20% to-[#EEEEEE] transition-colors duration-200",
             )}
           >
             <div className="absolute left-1/2 top-1/2 z-20 h-[814px] w-[376px] -translate-x-1/2 -translate-y-1/2">
