b840842b80817037726e896328eff0cf22e13532
vaunblu <vaunblu@proton.me>
2025-03-01 16:34:06 -0800

    update sidebar

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -6,6 +6,7 @@ import { Button } from "@/components/ui/button";
 import { ScrollArea } from "@/components/ui/scroll-area";
 
 const examples = [
+  "toggle-theme",
   "smooth-dropdown",
   "todo",
   "flashlight",
