8f894ea949e099672b67ca355308c8ff20b2c446
vaunblu <vaunblu@proton.me>
2025-04-11 06:08:21 -0700

    add manual tab

--- src/app/dynamic-tabs/page.tsx
+++ src/app/dynamic-tabs/page.tsx
@@ -28,13 +28,13 @@ function InnerContent() {
   const ctx = React.useContext(Context);
 
   return (
-    <div className="flex items-center rounded-full bg-[#fafafa] p-0.5 shadow-2xl">
+    <div className="flex items-center rounded-full bg-[#fafafa] p-0.5 text-lg shadow-xl">
       <motion.button
         onClick={() => ctx.setBgStatus("system")}
-        className={cn(
-          "relative h-full w-[175px] rounded-full py-2 text-black transition-colors",
-          ctx.bgStatus === "system" && "text-white",
-        )}
+        animate={{
+          color: ctx.bgStatus === "system" ? "#ffffff" : "#000000",
+        }}
+        className={cn("relative h-12 w-[175px] rounded-full")}
       >
         {ctx.bgStatus === "system" && (
           <motion.div
@@ -47,10 +47,7 @@ function InnerContent() {
 
       <motion.button
         onClick={() => ctx.setBgStatus("manual")}
-        className={cn(
-          "relative w-[175px] rounded-full py-2 text-black transition-colors",
-          ctx.bgStatus === "manual" && "py-0",
-        )}
+        className={cn("relative h-12 w-[175px] rounded-full")}
       >
         {ctx.bgStatus === "manual" && (
           <motion.div
@@ -60,46 +57,76 @@ function InnerContent() {
         )}
         <AnimatePresence key={ctx.bgStatus} mode="popLayout">
           {ctx.bgStatus === "system" ? (
-            <motion.span
-              initial={{ scale: 0.5, opacity: 0.5, filter: "blur(4px)" }}
-              animate={{ scale: 1, opacity: 1, filter: "blur(0px)" }}
-              exit={{ scale: 0.5, opacity: 0.5, filter: "blur(4px)" }}
-              className="relative z-10"
-            >
-              Manual
-            </motion.span>
-          ) : (
             <motion.div
-              initial={{ scale: 0.5, opacity: 0.5, filter: "blur(4px)" }}
-              animate={{ scale: 1, opacity: 1, filter: "blur(0px)" }}
-              exit={{ scale: 0.5, opacity: 0.5, filter: "blur(4px)" }}
-              transition={{ ...transition, delay: 0.1 }}
-              className="relative flex h-10 w-[175px] items-center rounded-full p-0.5"
+              key="manual-button"
+              initial={{ opacity: 0, color: "#000000" }}
+              animate={{
+                scale: 1,
+                opacity: 1,
+                color: "#000000",
+              }}
+              exit={{ opacity: 0, color: "#000000" }}
+              className="relative z-10 flex flex-col items-center gap-1"
             >
-              <motion.button
-                onClick={() => ctx.setStatus("light")}
-                className="relative h-full w-full rounded-full text-white"
+              <div className="h-[20px]">
+                <motion.span className="leading-[0]">Manual</motion.span>
+              </div>
+              <div className="flex items-center gap-1 text-xs">
+                <motion.span>Light</motion.span>
+                <span>/</span>
+                <motion.span>Dark</motion.span>
+              </div>
+            </motion.div>
+          ) : (
+            <>
+              <motion.div
+                key="manual-container"
+                initial={{ scale: 0.7, opacity: 0 }}
+                animate={{ scale: 1, opacity: 1 }}
+                exit={{ scale: 0.7, opacity: 0 }}
+                transition={{ ...transition, delay: 0.1 }}
+                className="relative flex h-12 w-[175px] items-center rounded-full p-0.5"
               >
-                {ctx.status === "light" && (
-                  <motion.div
-                    layoutId="active-pill"
-                    className="absolute inset-0 rounded-full bg-white mix-blend-difference"
-                  />
-                )}
-                <span>Light</span>
-              </motion.button>
-              <motion.button
-                onClick={() => ctx.setStatus("dark")}
-                className="relative h-full w-full rounded-full text-white"
-              >
-                {ctx.status === "dark" && (
-                  <motion.div
-                    layoutId="active-pill"
-                    className="absolute inset-0 rounded-full bg-white mix-blend-difference"
-                  />
-                )}
-                <span>Dark</span>
-              </motion.button>
+                <motion.button
+                  key="light-button"
+                  onClick={() => ctx.setStatus("light")}
+                  className="relative h-full w-full rounded-full text-white"
+                >
+                  {ctx.status === "light" && (
+                    <motion.div
+                      layoutId="active-pill"
+                      className="absolute inset-0 rounded-full bg-white mix-blend-difference"
+                    />
+                  )}
+                  <motion.span layoutId="light-text">Light</motion.span>
+                </motion.button>
+                <motion.button
+                  key="dark-button"
+                  onClick={() => ctx.setStatus("dark")}
+                  className="relative h-full w-full rounded-full text-white"
+                >
+                  {ctx.status === "dark" && (
+                    <motion.div
+                      layoutId="active-pill"
+                      className="absolute inset-0 rounded-full bg-white mix-blend-difference"
+                    />
+                  )}
+                  <motion.span layoutId="dark-text">Dark</motion.span>
+                </motion.button>
+              </motion.div>
+            </>
+          )}
+        </AnimatePresence>
+        <AnimatePresence>
+          {ctx.bgStatus === "manual" && (
+            <motion.div
+              key="manual-tab"
+              initial={{ left: "50%", x: "-50%", y: "100%" }}
+              animate={{ y: 0 }}
+              exit={{ left: "50%", x: "-50%", y: "100%" }}
+              className="absolute -top-6 -z-10 grid h-6 w-[135px] place-items-center rounded-t-2xl bg-[#fafafa] text-sm"
+            >
+              <motion.span>Manual</motion.span>
             </motion.div>
           )}
         </AnimatePresence>
@@ -127,7 +154,7 @@ export default function HomePage() {
       <MotionConfig transition={transition}>
         <GrainyBackground
           baseColor={
-            bgStatus === "system" || status === "light" ? "#ffffff" : "#aaaaaa"
+            bgStatus === "system" || status === "light" ? "#fafafa" : "#aaaaaa"
           }
           className="relative flex h-screen items-center justify-center transition-colors"
           grainOpacity={0.12}
